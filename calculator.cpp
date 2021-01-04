1a11a89253d3033d93930dd08219a8dbc1e9e5cb
yarikth <tkachenkoyaroslav123@gmail.com>
2021-01-04 23:21:07 +0200

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,32 @@
+#include "calculator.h"
+
+int Calculator::Add (double a, double b)
+{
+	return a + b + 0.5;
+}
+
+int Calculator::Sub (double a, double b)
+{
+    return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//10
+//11
+//12
+//13
+//14
+//15
---
+++ calculator.h
@@ -0,0 +1,11 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+        int Add (int, int);
+        int Mul (double, double);
+};
+
+#endif//CALCULATOR_H
